fb1774d88cf2562daff9ef75112e58504d1a3d25
evertonulisystem <evertonulisystem@gmail.com>
2021-07-25 21:09:58 -0300

    Inicio do Projeto

---
+++ spring-cadastro/src/main/java/br/gotasdetecnologia/SpringCadastroApplication.java
@@ -0,0 +1,13 @@
+package br.gotasdetecnologia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCadastroApplication.class, args);
+	}
+
+}
