760df8acdbc72fc611dbaca211a455be6fffcd73
evertonulisystem <evertonulisystem@gmail.com>
2021-07-25 21:37:15 -0300

    Primeiro mapeamento de Teste API

--- spring-cadastro/src/main/java/br/gotasdetecnologia/SpringCadastroApplication.java
+++ spring-cadastro/src/main/java/br/gotasdetecnologia/SpringCadastroApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringCadastroApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(SpringCadastroApplication.class, args);
+	
 	}
 
 }
---
+++ spring-cadastro/src/main/java/br/gotasdetecnologia/controller/TesteInicial.java
@@ -0,0 +1,20 @@
+package br.gotasdetecnologia.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TesteInicial {
+	
+	@RequestMapping(value = "/mostrarnome/{name}", method = RequestMethod.GET)
+	@ResponseStatus(HttpStatus.OK)
+	public String testeText(@PathVariable String name) {
+		return "Curso Spring REST" +name;
+		
+	}
+}
+
