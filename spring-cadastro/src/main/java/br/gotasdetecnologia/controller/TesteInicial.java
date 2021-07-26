3c89d03d6fd61064534da48d605546d3aced5de7
evertonulisystem <evertonulisystem@gmail.com>
2021-07-25 22:57:16 -0300

    Exemplificando a persistencia na pratica

--- spring-cadastro/src/main/java/br/gotasdetecnologia/controller/TesteInicial.java
+++ spring-cadastro/src/main/java/br/gotasdetecnologia/controller/TesteInicial.java
@@ -1,5 +1,6 @@
 package br.gotasdetecnologia.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,17 +8,29 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.gotasdetecnologia.repository.UsuarioRepository;
+import br.gotasdetecnologia.model.Usuario;
+
 @RestController
 public class TesteInicial {
 	
 	@RequestMapping(value = "/mostrarnome/{name}", method = RequestMethod.GET)
 	@ResponseStatus(HttpStatus.OK)
 	public String testeText(@PathVariable String name) {
-		return "Curso Spring REST" +name;
+		//return "Curso Spring REST" +name;
+		
+		Usuario usuario = new Usuario();
+		usuario.setNome(name);
+		
+		usuarioRepository.save(usuario);
+		
+		return "Usuario cadastrado" +name;
+		
 	}
 	
-
-
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 	
-}
+	
 
+}
