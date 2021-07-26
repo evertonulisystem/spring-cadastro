0459ecd1f375e903e867bdf607b9c5042fef784a
evertonulisystem <evertonulisystem@gmail.com>
2021-07-25 23:49:24 -0300

    Teste com cadastro (novo)

--- spring-cadastro/src/main/java/br/gotasdetecnologia/controller/TesteInicial.java
+++ spring-cadastro/src/main/java/br/gotasdetecnologia/controller/TesteInicial.java
@@ -1,10 +1,13 @@
 package br.gotasdetecnologia.controller;
-
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +24,7 @@ public class TesteInicial {
 		
 		Usuario usuario = new Usuario();
 		usuario.setNome(name);
+		//usuario.setIdade(40)
 		
 		usuarioRepository.save(usuario);
 		
@@ -30,7 +34,16 @@ public class TesteInicial {
 	
 	@Autowired
 	//Tem de colocar para mostrar a dependencia ("injeção")
+	//se tirar linha 25 não é encontrada
 	private UsuarioRepository usuarioRepository;
+	
+	@GetMapping(value="listatodos")
+	@ResponseBody //retorna os dados para o corpo da resposta em json
+	public ResponseEntity<List<Usuario>> listaUsuario(){
+		List<Usuario> usuarios = usuarioRepository.findAll();//executa consulta no BD
+		return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
+		
+	}
 	
 	
 
